a07bf5d78ae374ebaa319c92113477c3ed0a51c8
FedorSherbinin <fredersherb@gmail.com>
2024-02-28 23:12:18 +0300

    Notes application (Python)

--- view/NoteView.py
+++ view/NoteView.py
@@ -1,5 +1,9 @@
 class NoteView:
     def list_notes(self, notes):
+        if not notes:
+            print("Список заметок пуст.")
+            return
+
         for note in notes:
             print(f"{note.get('id')}. {note.get('title')} - {note.get('timestamp')}")
 
